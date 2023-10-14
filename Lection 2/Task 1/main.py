5d8b77b8889b31188de1f982ffcf68f5bdc76a77
Nazar_Kokish <146549458+N-Kokish@users.noreply.github.com>
2023-10-14 14:23:13 +0300

    main.py

--- Lection 2/Task 1/main.py
+++ Lection 2/Task 1/main.py
@@ -2,7 +2,7 @@ index = 1
 number = int(input("to see how much you want to keep sacred values = "))
 lst=[]
 while index < (number+1) :
-    element = input("ведить element {}= ".format(index))
+    element = input("leads element {}= ".format(index))
     lst.insert(index, element)
     index=index+1
 lst_set=list(set(lst))
