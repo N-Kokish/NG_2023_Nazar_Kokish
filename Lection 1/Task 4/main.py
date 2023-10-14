54b11aa6a12a1dcb9ab5fdcac7c40f94135ca73c
N-Kokish <nazarlev20@gmail.com>
2023-10-14 18:46:53 +0300

    Having added the corrected fourth task of the first lecture

--- Lection 1/Task 4/main.py
+++ Lection 1/Task 4/main.py
@@ -1,40 +1,26 @@
 
 znak = input("select operation(+,-,*,/,**,!**)")
-def sum(a,b):
-    a = int(input("a = "))
-    b = int(input("b = "))
-    return a+b
+number_1 = int(input("a = "))
+number_2 = int(input("b = "))
 match znak :
     case str("+"):
-        print("You selected add(a+b)")
-        a = int(input("a = "))
-        b = int(input("b = "))
-        print("sum "+str(a+b))
+        print("You selected add(a+b=)")
+        print("sum "+str(number_1+number_2))
     case str("-"):
-        print("You chose subtraction(a-b)")
-        a = int(input("a = "))
-        b = int(input("b = "))
-        print("difference "+str(a-b))
+        print("You chose subtraction(a-b=)")
+        print("difference "+str(number_1-number_2))
     case str("*"):
-        print("You chose multiplication(a*b)")
-        a = int(input("a = "))
-        b = int(input("b = "))
-        print("product "+str(a*b))
+        print("You chose multiplication(a*b=)")
+        print("product "+str(number_1*number_2))
     case str("/"):
-        print("You chose division(a/b)")
-        a = int(input("a = "))
-        b = int(input("b = "))
-        if b != 0 :
-         print("fraction "+str(a/b))
+        print("You chose division(a/b=)")
+        if number_2 != 0 :
+         print("fraction "+str(number_1/number_2))
         else:
             print("It is not possible to divide by 0, please choose the second number for repeated division")
     case str("**"):
-        print("You chose multiplication(a**b)")
-        a = int(input("a = "))
-        b = int(input("b = "))
-        print("raised to a power "+str(a**b))     
+        print("You chose multiplication(a**b=)")
+        print("raised to a power "+str(number_1**number_2))     
     case str("!**"):
-        print("You have chosen the operation of the roots of a number(a**1/b)")
-        a = int(input("a = "))
-        b = int(input("b = "))
-        print("The root of this number "+str(a**(1/b)))         
+        print("You have chosen the operation of the roots of a number(a**1/b=)")
+        print("The root of this number "+str(number_1**(1/number_2)))         
