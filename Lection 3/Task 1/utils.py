b914b3bd52ab831a43a32a5fe3705485746eaefb
N-Kokish <nazarlev20@gmail.com>
2023-10-21 15:41:42 +0300

    added 1 task of lesson 3

---
+++ Lection 3/Task 1/utils.py
@@ -0,0 +1,17 @@
+def drawLine():
+    print ("-"*25)
+file = open("test.txt", "a") # w - write, r - read, x - create, a - add to the end
+file.write("Helloiiii")
+file.close()
+
+# More interesting file write :3
+
+with open("test2.txt", "a") as f:
+    f.write ("Testing")
+print ("Goodbye :3")
+
+# Let's read multiple-lined file!
+
+with open("test3.txt", "r") as f:
+    for line in f.readlines():
+        print (line, end="")
