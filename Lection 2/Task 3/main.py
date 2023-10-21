8578d3045f325dbaac01d475825e4231661b2191
N-Kokish <nazarlev20@gmail.com>
2023-10-21 13:09:07 +0300

    added task 3 of lesson 2

--- Lection 2/Task 3/main.py
+++ Lection 2/Task 3/main.py
@@ -1,26 +1,24 @@
 def pop():
     print("---"*18)
 index = 1
-number = int(input("to see how much you want to keep sacred values = "))#число який вів користувач
+number = int(input("to see how much you want to keep sacred values = "))#the number entered by the use
 lst=[]
-l=[]
-p=0
-dilnik=1
+lst_prime_numbers=[]
+lst_dividers=[]
 numbers=0
-numb_dilniki=0
-while index < (number+1) :#запись чисел от 1 до числа який вів користувач
+while index < (number+1) :#entry of numbers from 1 to the number entered by the user in the list
     numbers = numbers+1
     lst.insert(index, numbers)
     index=index+1
-print(lst)
-for element_given in lst:#вивод списка в стовпчик
-    print(element_given)
 index=0
 while index<len(lst):
-    for i in range(1, int(len(lst))):
+    for i in range(1, int(len(lst)+1)):
      if (lst[index]) % i == 0:
-        print(str(i)+"  индексс {}".format(index))
-        l.append(i)
-        print(l)
-    l.clear()
-    index=index+1    
+        lst_dividers.append(i)
+    print("\n{} | ".format(index+1),lst_dividers)
+    if int(len(lst_dividers)) == 2:
+       lst_prime_numbers.append(index+1)
+    lst_dividers.clear()
+    index=index+1 
+pop()
+print("Prime numbers: {}".format(lst_prime_numbers) )      
