94513800582850e7b797d4127c8fa1865f8ef395
N-Kokish <nazarlev20@gmail.com>
2023-11-12 14:52:55 +0200

    1 task, 3 lectures, removed the extra term

--- Lection 3/Task 1/main.py
+++ Lection 3/Task 1/main.py
@@ -6,8 +6,6 @@ with open(text, "r") as f:
     for number_of_terms in f.readlines():
         for symbols in number_of_terms:
           lst.append(symbols)  
-          print(lst)
 number_of_each_character = collections.Counter(lst)#considers each element separately (as if a line or a word or a symbol)+ container is a subspecies of the Dictionary class
-print(number_of_each_character)
 print ("symbol : quantity")
 print (json.dumps(number_of_each_character,indent=4))
