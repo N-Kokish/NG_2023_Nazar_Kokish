a66b3fdf2255bb9dd9ff53566b667f734926db63
N-Kokish <nazarlev20@gmail.com>
2023-11-12 11:27:25 +0200

    now everything seems to have given 2 lectures in 1 assignment

--- Lection 2/Task 1/main.py
+++ Lection 2/Task 1/main.py
@@ -1,8 +1,4 @@
 index = 1
 lst=input().split()
-#while index < (number+1) :
-#    element = input("leads element {}= ".format(index))
- #   lst.insert(index, element)
-  #  index=index+1
 lst=set(lst)
 print("a list of unique values {}".format(lst))
