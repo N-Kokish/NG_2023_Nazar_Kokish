4713bba0cf43c4c6fc2a045f0c0e1da52aab846f
N-Kokish <nazarlev20@gmail.com>
2023-11-12 11:25:34 +0200

    from task 1 of the second lesson, now the user can enter as many numbers as he wants, I used .split()

--- Lection 2/Task 1/main.py
+++ Lection 2/Task 1/main.py
@@ -1,9 +1,8 @@
 index = 1
-number = int(input("to see how much you want to keep sacred values = "))
-lst=[]
-while index < (number+1) :
-    element = input("leads element {}= ".format(index))
-    lst.insert(index, element)
-    index=index+1
+lst=input().split()
+#while index < (number+1) :
+#    element = input("leads element {}= ".format(index))
+ #   lst.insert(index, element)
+  #  index=index+1
 lst=set(lst)
 print("a list of unique values {}".format(lst))
