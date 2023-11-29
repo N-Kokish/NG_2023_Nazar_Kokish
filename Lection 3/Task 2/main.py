49c51fb967b97692f709a80dc50ffd09e1384fc9
N-Kokish <nazarlev20@gmail.com>
2023-11-29 19:40:52 +0200

    Added a little more information about the PC in the 2nd task of the 3rd lecture

--- Lection 3/Task 2/main.py
+++ Lection 3/Task 2/main.py
@@ -1,3 +1,12 @@
 import platform
+import socket
+import psutil
+memory_info = {
+        'Total RAM': round(psutil.virtual_memory().total / (1024 ** 3), 2),
+        'Available RAM': round(psutil.virtual_memory().available / (1024 ** 3), 2),
+    }
+print(memory_info)
 print("processor: ",platform.processor())
 print("system release version: ",platform.uname())
+host_name = socket.gethostname()
+print("Ip :",socket.gethostbyname(host_name))
